210fbb7f627378c8a4ba4b02ef5e0516fff99f6f
Yohan Toledo <yohan.gabriel.bernart@gmail.com>
2021-10-26 00:44:49 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		
 
 		Department department = new Department(1, "Production");
-		System.out.println(department);
-		
+		Seller seller = new Seller(1, "yohan", "yohan@gmail.com", new Date() , 1500.00, department); 
+		System.out.println(seller);
 		
 	}
 
