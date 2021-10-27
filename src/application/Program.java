de4cd6b9e72940617172bba2dd78e3aa9eefb100
Yohan Toledo <yohan.gabriel.bernart@gmail.com>
2021-10-27 00:18:57 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,9 @@ public class Program {
 
 		Department department = new Department(1, "Production");
 		Seller seller = new Seller(1, "yohan", "yohan@gmail.com", new Date() , 1500.00, department); 
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 		
 	}
